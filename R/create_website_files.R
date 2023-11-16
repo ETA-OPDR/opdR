ec1fa5f34b7ed390e406bbee8440cd5502763e00
Kevin Reuss <reuss.kevin.l@dol.gov>
2023-11-16 02:05:19 -0500

    Fixed some errors.

--- R/create_website_files.R
+++ R/create_website_files.R
@@ -16,7 +16,7 @@ create_anonymous_files <- function(files_dir, year, product){
 
   set.seed(222)
   flist <- list.files(file_dir, full.names = TRUE, recursive = TRUE) |>
-    as.tibble() |>
+    as_tibble() |>
     filter(!str_detect(value, "anonymous")) |>
     mutate(state_file = basename(value)) |>
     # mutate(state_file = str_sub(value, start = -28)) |>
@@ -25,53 +25,68 @@ create_anonymous_files <- function(files_dir, year, product){
     mutate(newfile = paste0(output_dir, "/", state, "_", runif, ".html")) |>
     mutate(dol_link = paste0(state, "_", runif, ".html"))
 
-    file.copy(flist$value, flist$newfile)
+  file.copy(flist$value, flist$newfile, overwrite = TRUE)
 
-    if(product == "state_equity") {
-      drupal_location <- "DASP/state_equity/"
-    } else if(product == "state_assessments") {
-      drupal_location <- "DoP/WIOA_Annual_Reports/"
-    } else {
-      cat("No product was identified. \nThe product argument (i.e., product = ) must be indicated. \nThis function currently supports the following products: state_equity, state_assessments \nTo get your product added contact reuss.kevin.l@dol.gov")
-    }
+  if(product == "state_equity") {
+    drupal_location <- "DASP/state_equity/"
+  } else if(product == "state_assessments") {
+    drupal_location <- "DoP/WIOA_Annual_Reports/"
+  } else {
+    cat("No product was identified. \nThe product argument (i.e., product = ) must be indicated. \nThis function currently supports the following products: state_equity, state_assessments \nTo get your product added contact reuss.kevin.l@dol.gov")
+  }
 
-    file_info <- flist |>
-      mutate(program_year = year,
-             value = str_extract(value, "/OneDrive - US Department of Labor - DOL.*"),
-             newfile = str_extract(newfile, "/OneDrive - US Department of Labor - DOL.*")) |>
-      mutate(dol_link = paste0("https://www.dol.gov/sites/dolgov/files/ETA/opder/", drupal_location, year, "/", dol_link),
-             anonymous_name = paste0(state, "_", runif)) |>
-      rename(original_path = value,
-             anonymous_file_path = newfile,
-             DOL_website_link = dol_link) |>
-      relocate(state, program_year, state_file, original_path, anonymous_name, anonymous_file_path, DOL_website_link) |>
-      select(-runif)
+  file_info <- flist |>
+    mutate(program_year = year,
+           value = str_extract(value, "/OneDrive - US Department of Labor - DOL.*"),
+           newfile = str_extract(newfile, "/OneDrive - US Department of Labor - DOL.*")) |>
+    mutate(dol_link = paste0("https://www.dol.gov/sites/dolgov/files/ETA/opder/", drupal_location, year, "/", dol_link),
+           anonymous_name = paste0(state, "_", runif)) |>
+    rename(original_path = value,
+           anonymous_file_path = newfile,
+           DOL_website_link = dol_link) |>
+    relocate(state, program_year, state_file, original_path, anonymous_name, anonymous_file_path, DOL_website_link) |>
+    select(-runif)
 
-    class(file_info$DOL_website_link)<-"hyperlink" # mark as a hyperlink
-    wb_path <- here::here("reports", "state_file_info.xlsx")
+  class(file_info$DOL_website_link)<-"hyperlink" # mark as a hyperlink
+  wb_path <- here::here("reports", "state_file_info.xlsx")
 
-    wb <- wb_load(wb_path)
-    current_sheets <- getSheetNames(wb_path)
-    sheet_name <- paste0("PY", year, "_files")
+  sheet_name <- paste0("PY", year, "_files")
 
-    if (!sheet_name %in% current_sheets) {
-      addWorksheet(wb, sheetName = sheet_name)
-    }
-
-    writeData(wb, sheet_name, file_info)
+  if(file.exists(wb_path)){
+    wb <- loadWorkbook(wb_path)
+  } else {
+    wb <- createWorkbook()
     saveWorkbook(wb, wb_path, overwrite = TRUE)
+    wb <- loadWorkbook(wb_path)
+  }
 
-    if(product == "state_equity") {
-      select_office <- "DASP"
-    } else if(product == "state_assessments") {
-      select_office <- "DP"
-    } else {
-      cat("Error in identifying the office. \nTo get your product added contact reuss.kevin.l@dol.gov")
-    }
+  current_sheets <- getSheetNames(wb_path)
+  sheet_name <- paste0("PY", year, "_files")
 
-    copy_to_SP(wb_path, office = select_office)
+  # if (!sheet_name %in% current_sheets) {
+  #   addWorksheet(wb, sheetName = sheet_name)
+  # }
+
+  writeData(wb, sheet_name, file_info)
+
+  current_sheets <- getSheetNames(wb_path)
+  default_name <- "Sheet 1"
+  if (default_name %in% current_sheets) {
+    removeWorksheet(wb, sheet = 1)
+  }
+
+  saveWorkbook(wb, wb_path, overwrite = TRUE)
+
+  if(product == "state_equity") {
+    select_office <- "DASP"
+  } else if(product == "state_assessments") {
+    select_office <- "DP"
+  } else {
+    cat("Error in identifying the office. \nTo get your product added contact reuss.kevin.l@dol.gov")
+  }
+
+  copy_to_SP(wb_path, office = select_office)
 }
-
 
 
 

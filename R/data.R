942f866044ef1593b957876636205374548875a8
Kevin Reuss <reuss.kevin.l@dol.gov>
2025-01-24 09:49:09 -0500

    Added a documentation file for package data.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+
+
+#' State Information
+#'
+#' A dataset containing naming information for all states and territories.
+#' @format A data frame
+#' \describe{
+#' \item{Name}{The full name of the state or territory}
+#' \item{Alpha_code}{The abbreviation of the state or territory}
+#' \item{Status}{The type of area}
+#' \item{Region}{The ETA Regional Office of the area}
+#' }
+#' @source \url{}
+"state_info"

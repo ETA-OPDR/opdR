ad20cbb58572fa158c893fddadac199c3f688a5f
Kevin Reuss <reuss.kevin.l@dol.gov>
2025-05-12 16:43:49 -0400

    Small edit.

--- R/data_analysis.R
+++ R/data_analysis.R
@@ -19,13 +19,10 @@ create_wioa_program_columns <- function(data, program = "wioa") {
              dw = ifelse(p904 %in% 1:3 | p909 == 1, 1, NA),
              youth = ifelse(p905 %in% 1:3, 1, NA),
              wp = ifelse(p918 == 1, 1, NA))
-  } else if (program == "jc") {
+  } else if (program == "jc"){
     data <- data |>
       mutate(jc = 1)
   }
-
-
-
 
   cat("\nA column indicating if a participant was in each WIOA program was added.")
   return(data)

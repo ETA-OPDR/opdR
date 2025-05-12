93fbc9ca52ae7004967b59b803e359a634a534e3
Kevin Reuss <reuss.kevin.l@dol.gov>
2025-05-12 16:24:14 -0400

    Modified function to work with jc program data.

--- R/data_analysis.R
+++ R/data_analysis.R
@@ -12,12 +12,20 @@
 #' @import dplyr
 #'
 #' @export
-create_wioa_program_columns <- function(data) {
-  data <- data |>
-    mutate(adult = ifelse(p903 %in% 1:3, 1, NA),
-         dw = ifelse(p904 %in% 1:3 | p909 == 1, 1, NA),
-         youth = ifelse(p905 %in% 1:3, 1, NA),
-         wp = ifelse(p918 == 1, 1, NA))
+create_wioa_program_columns <- function(data, program = "wioa") {
+  if (program == "wioa"){
+    data <- data |>
+      mutate(adult = ifelse(p903 %in% 1:3, 1, NA),
+             dw = ifelse(p904 %in% 1:3 | p909 == 1, 1, NA),
+             youth = ifelse(p905 %in% 1:3, 1, NA),
+             wp = ifelse(p918 == 1, 1, NA))
+  } else if (program = "jc") {
+    data <- data |>
+      mutate(jc = 1)
+  }
+
+
+
 
   cat("\nA column indicating if a participant was in each WIOA program was added.")
   return(data)

61cbc4c6a706586d8e1c23353c91d6105399f9c0
Kevin Reuss <reuss.kevin.l@dol.gov>
2025-05-12 16:01:48 -0400

    Added RUCA data to package.

--- R/data.R
+++ R/data.R
@@ -12,3 +12,20 @@
 #' }
 #' @source \url{https://en.wikipedia.org/wiki/List_of_U.S._state_and_territory_abbreviations}
 "state_info"
+
+
+
+#' Rural-Urban Communting Area Codes
+#'
+#' A dataset containing codes on rural or urban by zip code.
+#' @format A data frame
+#' \describe{
+#' \item{ZIP_CODE}{The 5 digit zip code in a character format}
+#' \item{STATE}{The abbreviation of the state or territory}
+#' \item{ZIP_TYPE}{The type of zip code area}
+#' \item{RUCA1}{The primary RUCA codes}
+#' \item{RUCA2}{The secondary RUCA codes}
+#' }
+#' @source \url{https://www.ers.usda.gov/data-products/rural-urban-commuting-area-codes}
+"RUCA_zipcode"
+
